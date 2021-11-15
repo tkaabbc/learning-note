8feffc4cac6f87c4823157bf00bf872709e0d606
wangtingkeng <wangtingkeng@kezaihui.com>
2021-11-15 21:31:33 +0800

    feat: 增加数组扁平化

--- 09_数组扁平化.js
+++ 09_数组扁平化.js
@@ -1,1 +1,18 @@
-// todo
+// 法1: es6 的flat
+let a = [1,[2,3,[4,[5]]]];  
+a.flat(Infinity); // [1,2,3,4,5]  a是4维数组
+
+// 法2: 递归法
+function myFlat(arr) {
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (Array.isArray(element)) {
+      result = [...result, ...myFlat(element)]
+    } else {
+      result.push(element)
+    }
+  }
+  return result
+}
+console.log(myFlat(a));
